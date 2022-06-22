f98714cc6038db5ed1414aa71587fbc5e09712e5
Ruben da Luz <85204516+rubendaluz@users.noreply.github.com>
2022-06-22 17:25:46 +0100

    Add files via upload

--- index.c
+++ index.c
@@ -15,10 +15,11 @@ int menu(){
     int op;
 
     puts("MENU:");
-    puts("1-Inserir funcionario.");
-    puts("2-Contar funcionarios.");
-    puts("3-Listar funcionarios por salario.");
-    puts("0-Sair");
+    puts("\t1-Inserir funcionario.");
+    puts("\t2-Contar funcionarios.");
+    puts("\t-Listar funcionarios por salario.");
+    puts("\t0-Sair");
+    printf("\nSelecione>> ")
     fflush(stdin);
     scanf(" %d", &op);
     
@@ -27,66 +28,62 @@ int menu(){
     return op;
 }
 
-void inserir(FUNCIONARIO a[], int x, FILE *fp){
+void inserir(FILE *fp){
+	FUNCIONARIO funcionario;
     //Introdução dos dados do funcionário
     printf("Introduza o numero do funcionario: ");
-    scanf("%d", &a[x].num_funcionario);
+    scanf("%d", &funcionario.num_funcionario);
     printf("Introduza o nome do funcionario: ");
-    scanf("%s",a[x].nome);
+    scanf("%s", funcionario.nome);
     printf("Introduza o salario ganho pelo funcionario: ");
-    scanf("%f", &a[x].salario);
+    scanf("%f", &funcionario.salario);
     //Gravar os mesmos dados no ficheiro binario
-    fwrite(a,sizeof(FUNCIONARIO),1,fp);
+    fseek(fp,0L,SEEK_END);
+    if(fwrite(&funcionario,sizeof(FUNCIONARIO),1,fp) != 1){
+    	printf("Erro ao gravar dados do funcionario.\n");
+	};
 }
 
-int contagem(FUNCIONARIO a[], FILE *fp){
+int contagem(FILE *fp){
     fseek(fp,0L,SEEK_END);
     int n_fun = ftell(fp) / sizeof(FUNCIONARIO);
 
     return n_fun; 
 }
 
-void ordenar(FILE *fp, int x){
-    FUNCIONARIO b[x];
-    int i, j, min, aux1;
-    char aux2[20];
-    float aux3;
+void lerFicheiro(FILE *fp, int qtd, FUNCIONARIO funcionarios[]){
+	rewind(fp);
+	if((fread(funcionarios,sizeof(FUNCIONARIO),qtd,fp)) != qtd){
+		printf("Erro na leitura dos dados do ficheiro.\n");
+	}
+}
 
-
-    fseek(fp,0l,SEEK_SET);
-    fread(b,sizeof(FUNCIONARIO),x,fp);
-
+void ordenar(FUNCIONARIO funcionarios[], int x){
+    FUNCIONARIO aux;
+    int min, i, j;
     
-    for (i = 0; i < x-1; i++) {
+    
+    for ( i = 0; i < x-1; i++) {
         min = i;
         for (j = i+1; j < x; j++) {
-            if (b[j].salario > b[min].salario)
+            if (funcionarios[j].num_funcionario < funcionarios[min].num_funcionario)
             min = j;
         }
         if (min != i){ 
-            // troca do numero de funcionario
-            aux1 = b[i].num_funcionario;
-            b[i].num_funcionario = b[min].num_funcionario;
-            b[min].num_funcionario = aux1;
-            //troca do nome de funcionario
-            strcpy(aux2,b[i].nome);
-            strcpy(b[i].nome,b[min].nome);
-            strcpy(b[min].nome,aux2);
-            //troca do salario
-            aux3 = b[i].salario;
-            b[i].salario = b[min].salario;
-            b[min].salario = aux3;
+            aux = funcionarios[i];
+            funcionarios[i] = funcionarios[min];
+            funcionarios[min] = aux;
         }
     }
     printf("NUMERO \tNOME \t\tSALARIO\n");
     for(i = 0; i < x;i++){
-        fprintf(stdout,"%d  \t%s  \t\t%.2f\n", b[i].num_funcionario,b[i].nome,b[i].salario);
+        printf("%d  \t%s  \t\t%.2f\n", funcionarios[i].num_funcionario, funcionarios[i].nome, funcionarios[i].salario);
     }
 }
 
 int main (){
 
-    int i,j, opcao;
+    int qtd, opcao;
     FUNCIONARIO funcionarios[MAX];
     FILE *fout;
 
@@ -96,29 +93,32 @@ int main (){
     }
 
 
-    while (1)
+    do
     {
        opcao =  menu();
        switch (opcao)
        {
        case 1:
-           i = 0;
-           inserir(funcionarios,i,fout);
-           i++;
+           inserir(fout);
            break;
        case 2:
-           j = contagem(funcionarios,fout); 
-           printf("Foram insiridos %d funcionarios no ficheiro.\n", j); 
+           qtd = contagem(fout); 
+           printf("Foram insiridos %d funcionarios no ficheiro.\n", qtd); 
            break;
        case 3:
-           j = contagem(funcionarios,fout); 
-           ordenar(fout,j);	
+           qtd = contagem(fout); 
+           lerFicheiro(fout,qtd,funcionarios);
+           ordenar(funcionarios,qtd);	
            break;
+        case 0:
+        	printf("Programa encerrado\n");
+        	fclose(fout);
+        	break;
        default:
             puts("Opcao inserida eh invalida!!!");
             break;
        }
-    }
+    }while(opcao != 0);
     
     fclose(fout);
     return 0;
